355da1c43c57e37d73246abc5472a4ad49a78c12
ajitkumarjena11 <158299220+ajitkumarjena11@users.noreply.github.com>
2024-03-01 18:08:12 +0530

    commited..

---
+++ maps/src/p1/Map.java
@@ -0,0 +1,18 @@
+package p1;
+
+import java.util.HashMap;
+
+public class Map {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+    
+		HashMap<Integer,String>hs =new HashMap<>();
+		hs.put(1,"rahul");
+		hs.put(2, "shashi");
+		hs.put(3, "abhi");
+		hs.entrySet().stream().sorted(Map.)
+		
+	}
+
+}
